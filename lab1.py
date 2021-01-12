37ee1fa256f138ccc909a4b048484589c18ed7bf
rodann42 <60013635+rodann42@users.noreply.github.com>
2021-01-11 18:05:10 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -5,3 +5,7 @@ print(requests.__version__)
 
 # get google homepage
 response = requests.get("http://google.com")
+
+
+gitResponse = requests.get("https://raw.githubusercontent.com/kobitoko/cmput404lab1/master/lab1.py")
+print gitResponse.text
