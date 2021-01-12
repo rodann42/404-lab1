1b8f38ea4dbec6332b8b9c4c05e1ea6273cd8d21
rodann42 <60013635+rodann42@users.noreply.github.com>
2021-01-11 18:06:26 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -7,5 +7,6 @@ print(requests.__version__)
 response = requests.get("http://google.com")
 
 
-gitResponse = requests.get("https://raw.githubusercontent.com/kobitoko/cmput404lab1/master/lab1.py")
-print gitResponse.text
+# get script from github
+gitResponse = requests.get("https://raw.githubusercontent.com/rodann42/404-lab1/master/lab1.py")
+print(gitResponse.text)
